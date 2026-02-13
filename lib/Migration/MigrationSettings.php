acfd6a944af3f8e2865bdebb2c3011c810886471
Mohamed-Ismail Mejri <imejri@hotmail.com>
2026-02-13 15:09:34 +0100

    Create MigrationSettings.php for Nextcloud migration class

---
+++ lib/Migration/MigrationSettings.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Migration;
+
+class MigrationSettings {
+    public static function migrateSettings(array $settings) {
+        $migratedSettings = [];
+
+        foreach ($settings as $key => $value) {
+            // Assume settings are delimited by commas
+            $migratedSettings[$key] = explode(',', $value);
+        }
+
+        return json_encode($migratedSettings);
+    }
+}
